e949d6e46302224f96e8cd9b57170382011b1ccc
Milan Proell <milan.proell@googlemail.com>
2019-07-17 17:30:34 +0200

    getting into the functional vibe

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,19 @@
 use git2::Repository;
+use git2::Object;
+use git2::Tree;
+use git2::Diff;
+use git2::DiffDelta;
 
 fn main() {
-    let repo = match Repository::open(".") {
-        Ok(repo) => repo,
-        Err(e) => panic!("failed to open: {}", e),
-    };
-
-    let rev = match repo.revparse_single("HEAD~1") {
-        Ok(rev) => rev,
-        Err(e) => panic!("failed to parse Rev HEAD~1: {}", e),
-    };
-
-    let commit = match rev.as_commit() {
-        Some(commit) => commit,
-        None => panic!("failed to grab Commit"),
-    };
-
-    let tree = match commit.tree() {
-        Ok(tree) => tree,
-        Err(e) => panic!("could not grab Tree for Commit: {}", e),
-    };
-
-    let diff = match repo.diff_tree_to_workdir_with_index(Some(&tree), None) {
-        Ok(diff) => diff,
-        Err(e) => panic!("failed to get Diff: {}", e),
-    };
-
-    if let Err(e) = diff.foreach(
-        &mut |file, _progress| {
-            println!(
+    let repo = Repository::open(".").expect("Repository not found");
+    let diff: Diff = repo.revparse_single("HEAD^{tree}")
+        .and_then(|rev: Object| rev.peel_to_tree())
+        .and_then(|tree: Tree| repo.diff_tree_to_workdir_with_index(Some(&tree), None))
+        .expect("Creating diff from tree to working dir not possible");
+    
+    diff.deltas().for_each(|file: DiffDelta| println!(
                 "File: {:?} Change type: {:?}",
                 file.old_file().path(),
                 file.status()
-            );
-            true
-        },
-        None,
-        None,
-        None,
-    ) {
-        panic!("failed to list Files: {}", e);
-    };
+            ));
 }
