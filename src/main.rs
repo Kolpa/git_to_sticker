e285e8e7d646f81647d39f72ed2e680517b90277
Kolya Opahle <kolyaopahle@gmail.com>
2019-07-18 00:01:40 +0200

    Ooofff my rust hurts

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,63 @@
-use git2::Diff;
-use git2::DiffDelta;
-use git2::Object;
-use git2::Repository;
-use git2::Tree;
+use git2::{Delta, Diff, DiffDelta, DiffFile, Object, Repository, Tree};
+use serde::Deserialize;
+use std::ffi::OsStr;
+use std::fs::File;
+use std::path::Path;
+
+#[derive(Deserialize)]
+struct Sticker {
+    file: String,
+    emoji: String,
+}
+
+#[derive(Deserialize)]
+struct Stickers {
+    stickers: Vec<Sticker>,
+}
+
+fn file_is_png(file: DiffFile) -> bool {
+    file.path().map_or(false, |path: &Path| {
+        path.extension().map_or(false, |ext: &OsStr| ext.eq("png"))
+    })
+}
+
+//TODO: This is quite Broken
+fn resolve_sticker_for_image(path: &Path, stickers_obj: &Stickers) -> Option<Sticker> {
+    for sticker in stickers_obj.stickers {
+        if path
+            .file_name()
+            .map_or(false, |name: &OsStr| name.eq(sticker.file.as_str()))
+        {
+            return Some(sticker);
+        }
+    }
+
+    None
+}
 
 fn main() {
     let repo = Repository::open(".").expect("Repository not found");
+
     let diff: Diff = repo
         .revparse_single("HEAD~1^{tree}")
         .and_then(|rev: Object| rev.peel_to_tree())
         .and_then(|tree: Tree| repo.diff_tree_to_workdir_with_index(Some(&tree), None))
         .expect("Creating diff from tree to working dir not possible");
 
-    diff.deltas().for_each(|file: DiffDelta| {
-        println!(
-            "File: {:?} Change type: {:?}",
-            file.old_file().path(),
-            file.status()
-        )
-    });
+    let pngs = diff
+        .deltas()
+        .filter(|delta: &DiffDelta| file_is_png(delta.new_file()));
+
+    let file: File = File::open("stickers.json")
+        .expect("Could not open File stickers.json");
+    let stickers: Stickers = serde_json::from_reader(file)
+        .expect("Could not parse File stickers.json");
+
+    for png in pngs {
+        if png.status() == Delta::Added {
+            let filePath: &Path = png.new_file().path().expect("Could not get File Path from Diff");
+
+            resolve_sticker_for_image(filePath, &stickers);
+        }
+    }
 }
