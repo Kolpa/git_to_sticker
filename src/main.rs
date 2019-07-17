fc37d8c8d4d7fb382a9be4d026c52e78f5b92a84
Milan Proell <milan.proell@googlemail.com>
2019-07-17 18:48:08 +0200

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-use git2::Repository;
-use git2::Object;
-use git2::Tree;
 use git2::Diff;
 use git2::DiffDelta;
+use git2::Object;
+use git2::Repository;
+use git2::Tree;
 
 fn main() {
     let repo = Repository::open(".").expect("Repository not found");
-    let diff: Diff = repo.revparse_single("HEAD~1^{tree}")
+    let diff: Diff = repo
+        .revparse_single("HEAD~1^{tree}")
         .and_then(|rev: Object| rev.peel_to_tree())
         .and_then(|tree: Tree| repo.diff_tree_to_workdir_with_index(Some(&tree), None))
         .expect("Creating diff from tree to working dir not possible");
-    
-    diff.deltas().for_each(|file: DiffDelta| println!(
-                "File: {:?} Change type: {:?}",
-                file.old_file().path(),
-                file.status()
-            ));
+
+    diff.deltas().for_each(|file: DiffDelta| {
+        println!(
+            "File: {:?} Change type: {:?}",
+            file.old_file().path(),
+            file.status()
+        )
+    });
 }
